7312a042ec01043b50f8480404652b0774662caa
3T5K <ivank.dbt@gmail.com>
2026-08-18 19:43:21 +0200

    config: changed colors and font

--- config/general.h
+++ config/general.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "CaskaydiaCoveNerdFont:size=13", "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -14,8 +14,8 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { "#cba6f7", "#1e1e2e", "#595959" },
+	[SchemeSel]  = { "#1e1e2e", "#cba6f7", "#cba6f7" },
 };
 
 /* tagging */
