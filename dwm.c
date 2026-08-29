507ad2bff9c2f09873014c6187c2aa7ff28bce1c
3T5K <ivank.dbt@gmail.com>
2026-08-29 20:13:13 +0200

    tmpnmaster-6.8

--- dwm.c
+++ dwm.c
@@ -67,6 +67,12 @@ enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
        ClkClientWin, ClkRootWin, ClkLast }; /* clicks */
 enum { XrayIn, XrayAc };
 enum { XrayGrpNone, XrayGrpTileMaster, XrayGrpTileStack };
+enum {
+    TmpNmRespectNmaster     = 1 << 0,
+    TmpNmHookUnmanage       = 1 << 1,
+    TmpNmHookToggleFloating = 1 << 2,
+    TmpNmHookSetFullscreen  = 1 << 3,
+};
 
 typedef union {
 	int i;
@@ -234,6 +240,7 @@ static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
 static TileInfo tileinfo(void);
+static void tmpnmdec(Client *c, int arg);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
@@ -1673,6 +1680,7 @@ setfullscreen(Client *c, int fullscreen)
 	if (fullscreen && !c->isfullscreen) {
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
+        tmpnmdec(c, TmpNmHookSetFullscreen);
 		c->isfullscreen = 1;
 		c->oldstate = c->isfloating;
 		c->oldbw = c->bw;
@@ -1975,6 +1983,25 @@ tileinfo(void)
 }
 
 void
+tmpnmdec(Client *c, int arg)
+{
+    Client *i;
+    int nc, si;
+
+    if (!c || !c->mon || !(tmpnmcfg & arg) || c->isfloating || !ISVISIBLE(c))
+        return;
+
+    for (nc = si = 0, i = nexttiled(c->mon->clients); i; ++nc, i = nexttiled(i->next))
+        if (i == c)
+            si = nc;
+    c->mon->nmaster
+        = (nc == 1 && (tmpnmcfg & TmpNmRespectNmaster)) ? nmaster
+        : (nc < c->mon->nmaster) ? nc - (nc > MAX(tmpnmdectil, 0))
+        : c->mon->nmaster - (si < c->mon->nmaster
+                && c->mon->nmaster > MAX(tmpnmdectil, 0));
+}
+
+void
 togglebar(const Arg *arg)
 {
 	selmon->showbar = selmon->pertag->showbars[selmon->pertag->curtag] = !selmon->showbar;
@@ -1990,6 +2017,7 @@ togglefloating(const Arg *arg)
 		return;
 	if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
 		return;
+    tmpnmdec(selmon->sel, TmpNmHookToggleFloating);
 	selmon->sel->isfloating = !selmon->sel->isfloating || selmon->sel->isfixed;
 	if (selmon->sel->isfloating) {
         xraycfg(selmon->sel, XrayGrpNone, -1);
@@ -2070,6 +2098,7 @@ unmanage(Client *c, int destroyed)
 	Monitor *m = c->mon;
 	XWindowChanges wc;
 
+    tmpnmdec(c, TmpNmHookUnmanage);
 	detach(c);
 	detachstack(c);
 	if (!destroyed) {
