e2881e67fbcb8d9efa51c29b5034fe78cc70b97f
3T5K <ivank.dbt@gmail.com>
2026-08-29 20:13:13 +0200

    tmpnmaster-6.8

--- config.def.h
+++ config.def.h
@@ -18,6 +18,15 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const int tmpnmdectil = 1;
+
+static const int tmpnmcfg
+    = TmpNmRespectNmaster
+    | TmpNmHookUnmanage
+    | TmpNmHookToggleFloating
+    | TmpNmHookSetFullscreen
+    ;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
