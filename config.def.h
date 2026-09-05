f2c520ea9af63c39b746d5a5aab4bd86adb83fae
3T5K <ivank.dbt@gmail.com>
2026-09-06 01:46:32 +0200

    tilefocus-6.8

--- config.def.h
+++ config.def.h
@@ -18,6 +18,16 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const TileFocusCallback tilefocusfallback
+    = tilefocusdefaultfallback;
+
+static const int tilefocuscfg
+    = TileFocusMasterLast
+    | TileFocusStackLast
+    | TileFocusVertOverflow
+    | TileFocusHorzOverflow
+    ;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -66,12 +76,14 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_h,      tilefocus,      {.i = 'W' } },
+    { MODKEY,                       XK_j,      tilefocus,      {.i = 'S' } },
+    { MODKEY,                       XK_k,      tilefocus,      {.i = 'N' } },
+    { MODKEY,                       XK_l,      tilefocus,      {.i = 'E' } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
     { MODKEY,                       XK_x,      exstack,        {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
