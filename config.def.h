7366ba04ad5384c4e938912b4a6227961d638f7f
3T5K <ivank.dbt@gmail.com>
2026-09-17 23:06:17 +0200

    tileffb-6.8

--- config.def.h
+++ config.def.h
@@ -28,6 +28,8 @@ static const int tmpnmcfg
     | TmpNmHookSetFullscreen
     ;
 
+static const TileFFBCallback tileffbfallback = NULL;
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
--- dwm.c
+++ dwm.c
@@ -160,6 +160,8 @@ typedef struct {
 	int monitor;
 } Rule;
 
+typedef Client *(*TileFFBCallback)(Client *);
+
 typedef struct {
     Client *bs[2]; /* bs[0]: bottom master client bs[1]: bottom stack client */
     Client *ts[2]; /* ts[0]: top master client    ts[1]: top stack client    */
@@ -243,6 +245,7 @@ static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
+static Client *tileffb(Client *c);
 static TileInfo tileinfo(void);
 static void tmpnmdec(Client *c, int arg);
 static void togglebar(const Arg *arg);
@@ -1958,6 +1961,51 @@ tileinfo(void)
     return ti;
 }
 
+Client *
+tileffb(Client *c)
+{
+    TileInfo ti;
+    Client *x, *y;
+    int i;
+
+    if (selmon->lt[selmon->sellt]->arrange != &tile)
+        return tileffbfallback ? tileffbfallback(c) : NULL;
+
+    if (!c || selmon->sel != c)
+        return NULL;
+
+    if (c->isfloating) {
+        for ( x = c->mon->stack
+            ; x && (!x->isfloating || !ISVISIBLE(x) || x == c)
+            ; x = x->snext
+            );
+        return x;
+    }
+
+    ti = tileinfo();
+    if (ti.nc == 1)
+        return NULL;
+
+    if (ti.xs[ti.st]) {
+        for (x = selmon->stack; x; x = x->snext)
+            if (ISVISIBLE(x) && !x->isfloating && x != c) {
+                for ( i = 0, y = nexttiled(selmon->clients)
+                    ; y && y != x
+                    ; y = nexttiled(y->next), ++i
+                    );
+                if ((int[2]){ i <  selmon->nmaster
+                            , i >= selmon->nmaster }[ti.st]) {
+                    return x;
+                }
+            }
+    }
+
+    if (ti.st && ti.xs[0] && selmon->sel == ti.ts[1] && selmon->sel == ti.bs[1])
+        return ti.xs[0];
+
+    return ti.cs[1] ? ti.cs[1] : ti.cs[0];
+}
+
 void
 tmpnmdec(Client *c, int arg)
 {
@@ -2064,9 +2112,11 @@ unfocus(Client *c, int setfocus)
 void
 unmanage(Client *c, int destroyed)
 {
+    Client *nc = NULL;
 	Monitor *m = c->mon;
 	XWindowChanges wc;
 
+    nc = tileffb(c);
     tmpnmdec(c, TmpNmHookUnmanage);
 	detach(c);
 	detachstack(c);
@@ -2083,7 +2133,7 @@ unmanage(Client *c, int destroyed)
 		XUngrabServer(dpy);
 	}
 	free(c);
-	focus(NULL);
+	focus(nc);
 	updateclientlist();
 	arrange(m);
 }
